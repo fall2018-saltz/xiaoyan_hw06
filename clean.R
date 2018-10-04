5fa2cb277b3bc7fdd95380cb0fac47d598e6e070
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:39:02 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,16 @@
 
 clean_data <- raw_data
+
+readData <- function(states){ 
+  dfStatesRaw <- read.csv(url(urlToRead), stringsAsFactors=FALSE)
+  dfStates <- dfStatesRaw[c(-1,-53),-1:-3]
+  dfStatesFinal <- dfStates2[,-1]
+  View(dfStates2)
+  colnames(dfStates2)
+  col.names <- colnames(dfStates2)
+  col.names
+  col.names <- c("stateName","population","popOver18","percentOver18")
+  col.names
+  colnames(dfStates2) <- col.names
+  return(dfStates2)
+}
