8ccbb1689a853f9a1a56a8b28eadde5a21498460
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:40:43 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,9 +2,7 @@
 clean_data <- raw_data
 
 readData <- function(states){ 
-  dfStatesRaw <- read.csv(url(urlToRead), stringsAsFactors=FALSE)
-  dfStates <- dfStatesRaw[c(-1,-53),-1:-3]
-  dfStatesFinal <- dfStates2[,-1]
+  states2 <- states[-1,]
   View(dfStates2)
   colnames(dfStates2)
   col.names <- colnames(dfStates2)
