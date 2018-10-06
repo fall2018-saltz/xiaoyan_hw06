0cba1c2e9445474ffb0815d208c7a8bd3fb4047d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 12:14:44 -0400

    Saving mostMurders.R

--- mostMurders.R
+++ mostMurders.R
@@ -1,3 +1,4 @@
 
 censusArrestsFinal2 <-censusArrestsFinal
-censusArrestsFinal2$murderNumByState 
+
+censusArrestsFinal2$murderNumByState <- censusArrestsFinal$Murder/100000*censusArrestsFinal$population
