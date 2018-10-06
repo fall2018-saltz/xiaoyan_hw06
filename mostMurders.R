9270b4c2a0eb3abb79515f9fb8e4fe750b5d4a46
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 12:19:36 -0400

    Saving mostMurders.R

--- mostMurders.R
+++ mostMurders.R
@@ -3,3 +3,8 @@ censusArrestsFinal2 <-censusArrestsFinal
 
 censusArrestsFinal2$murderNumByState <- censusArrestsFinal$Murder/100000*censusArrestsFinal$population
 
+ barC <- ggplot(censusArrestsFinal2,aes(x=reorder(stateName, censusArrestsFinal$Murder), y=july11pop,fill=percentChange)) 
+ g <- g + geom_col() 
+ g <- g + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ g
+ 
