3f49fb7b4a29e2e76812b44622a22f256f4e517c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 13:06:19 -0400

    Saving script4942.R

--- script4942.R
+++ script4942.R
@@ -1,7 +1,7 @@
 
 library("ggplot2")
-  barCColor <- ggplot(censusArrestsFinal2, aes(x=population, y=popOver18)) +
+ScatterPlot <- ggplot(censusArrestsFinal2, aes(x=population, y=popOver18)) +
     geom_point(aes(size=Murder, color=Murder))+
     theme(axis.text.x = element_text(angle = 90))
- barCColor
- 
+
+ScatterPlot
