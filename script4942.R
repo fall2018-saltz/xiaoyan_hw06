66df14ba9230c32b9c1f96de0675c50432ba3561
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 13:03:07 -0400

    Saving script4942.R

--- script4942.R
+++ script4942.R
@@ -1,1 +1,11 @@
 
+  barCColor <- ggplot(censusArrestsFinal2, aes(x=population, y=popOver18)) +
+    geom_point(aes(size=Murder, color=Murder,fill=percentOver18))+labs(x="Total Murders")+
+    theme(axis.text.x = element_text(angle = 90))
+ barCColor
+ 
+ head(censusArrestsFinal2)
+  g <- ggplot(dfStates, aes(x=popChange, y=percentChange))  
+ g <- g + geom_point(aes(size=july11pop, color=july11pop))
+ g
+ 
