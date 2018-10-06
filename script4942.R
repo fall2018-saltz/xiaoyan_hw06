8afdfb56059a3e58f91c04e4a4c57fe405c3d980
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 13:09:50 -0400

    Saving script4942.R

--- script4942.R
+++ script4942.R
@@ -1,7 +1,7 @@
 
 library("ggplot2")
-ScatterPlot <- ggplot(censusArrestsFinal, aes(x=population, y=popOver18)) +
+scatterPlot <- ggplot(censusArrestsFinal, aes(x=population, y=popOver18)) +
     geom_point(aes(size=Murder, color=Murder))+
     theme(axis.text.x = element_text(angle = 90))
 
-ScatterPlot
+scatterPlot
