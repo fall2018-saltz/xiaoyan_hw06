de2853f7ef215844041f7a1738af90f2277a7827
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 13:13:21 -0400

    Saving script9114.R

--- script9114.R
+++ script9114.R
@@ -1,6 +1,6 @@
 
 library("ggplot2")
-scatterPlot <- ggplot(censusArrestsFinal, aes(x=population, y=popOver18,size=Murder, color=Murder)) +
+scatterPlot2 <- ggplot(censusArrestsFinal, aes(x=population, y=popOver18,size=Murder, color=Murder)) +
     geom_point()
  
-scatterPlot
+scatterPlot2
