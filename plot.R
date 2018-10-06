9b005f01d00a4ede18f723b8a45168dffcbd757f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 10:52:46 -0400

    Saving plot.R

--- plot.R
+++ plot.R
@@ -1,4 +1,6 @@
 
+library("ggplot")
+ 
  g <- ggplot(dfStates, aes(x=july11pop)) 
  g <- g + geom_histogram(binwidth=5000000, color="black", fill="white")
  g <- g + ggtitle("states population histogram")
