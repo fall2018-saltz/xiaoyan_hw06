5356f16b0b0ed981754d7372295f463d9d36a8c6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 12:38:29 -0400

    Saving mostMurders.R

--- mostMurders.R
+++ mostMurders.R
@@ -5,13 +5,13 @@ censusArrestsFinal2 <-censusArrestsFinal
 censusArrestsFinal2$murderNumByState <- censusArrestsFinal$Murder/100000*censusArrestsFinal$population
 
 library("ggplot2")
- barCMostMurder <- ggplot(censusArrestsFinal2, title="Most Murders") +
+ barCMostMurders <- ggplot(censusArrestsFinal2, title="Most Murders") +
     geom_col(aes(x=stateName, y=murderNumByState))+labs(x="Total Murders")+
     theme(axis.text.x = element_text(angle = 45))
- barCMostMurder
+ barCMostMurders
  
- barCSortedMostMurder <- ggplot(censusArrestsFinal2, title="Most Murders") +
+ barCSortedMostMurders <- ggplot(censusArrestsFinal2, title="Most Murders") +
     geom_col(aes(x=reorder(stateName,murderNumByState), y=murderNumByState))+labs(x="Total Murders")+
     theme(axis.text.x = element_text(angle = 45))
- barCSortedMostMurder
+ barCSortedMostMurders
  
