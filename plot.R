acf4a163d104e54fe807f400762e8918ae03afa1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 10:58:32 -0400

    Saving plot.R

--- plot.R
+++ plot.R
@@ -4,7 +4,12 @@ library("ggplot2")
 head(censusArrestsFinal,10)
 
  
- g <- ggplot(censusArrestsFinal, aes(x=population)) 
- g <- g + geom_histogram(binwidth=5000000, color="black", fill="white")
- g <- g + ggtitle("states population histogram")
- g
+ g1.1 <- ggplot(censusArrestsFinal, aes(x=population)) 
+ g1.2 <- g1.1 + geom_histogram(binwidth=5000000, color="black", fill="white")
+ g1 <- g1.2 + ggtitle("States Population Histogram")
+ g1
+
+ g2.1 <- ggplot(censusArrestsFinal, aes(x=Murder)) 
+ g2.2 <- g2.1 + geom_histogram(binwidth=5000000, color="black", fill="white")
+ g2 <- g2.2 + ggtitle("Murder Rate Histogram")
+ g2
