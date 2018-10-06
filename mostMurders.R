a4c546ce477f59ac6b868dd045c40d61411df42b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 12:07:00 -0400

    Saving mostMurders.R

--- mostMurders.R
+++ mostMurders.R
@@ -1,4 +1,5 @@
 
 murderNumByState <- censusArrestsFinal$Murder/100000*censusArrestsFinal$population
-censusArrestsFinal2 <- data.frame(murderNumByState,colnames(censusArrestsFinal))
+cNames<-c(murderNumByState,colnames(censusArrestsFinal))
+censusArrestsFinal2 <- data.frame(cNames)
 str(censusArrestsFinal)
