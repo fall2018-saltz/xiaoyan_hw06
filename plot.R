0a9f7182e5eff1d3fcefba7a4d087a42b23fe230
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 11:10:07 -0400

    Saving plot.R

--- plot.R
+++ plot.R
@@ -10,6 +10,6 @@ head(censusArrestsFinal,10)
  g1
 
  g2.1 <- ggplot(censusArrestsFinal, aes(x=Murder)) 
- g2.2 <- g2.1 + geom_histogram(binwidth=5, color="black", fill="white")
+ g2.2 <- g2.1 + geom_histogram(color="black", fill="white")
  g2 <- g2.2 + ggtitle("Murder Rate Histogram")
  g2
