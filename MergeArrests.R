616465c29fe9cba96382febee5b073f5fdfdcf45
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 10:43:32 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -5,8 +5,6 @@ rownames(cleanCensus) <- cleanCensus$stateName
 rownames(cleanCensus) 
 str(cleanCensus)
 
-mergedData <- merge(cleanCensus,arrests,by= "row.names", all= TRUE)
+censusArrests <- merge(cleanCensus,arrests,by= "row.names", all= TRUE)
 
-censusArrests <- mergedData
-
-str(censusArrests)
+head(censusArrests)
