126b51985689e259286be2581fdd27fc6e702849
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 11:21:14 -0400

    Saving plot.R

--- plot.R
+++ plot.R
@@ -16,11 +16,16 @@ head(censusArrestsFinal,10)
 
 
 hisAssa1 <- ggplot(censusArrestsFinal, aes(x=Murder)) 
- hisAssa2 <- hisAssa1 + geom_histogram(binwidth=2,color="black", fill="white")
- hisAssa <- hisAssa2 + ggtitle("Murder Rate Histogram")
+ hisAssa2 <- hisAssa1 + geom_histogram(binwidth=20,color="black", fill="white")
+ hisAssa <- hisAssa2 + ggtitle("Assault Rate Histogram")
  hisAssa
  
  hisUrbanPop1 <- ggplot(censusArrestsFinal, aes(x=Murder)) 
  hisUrbanPop2 <- hisUrbanPop1 + geom_histogram(binwidth=2,color="black", fill="white")
- hisUrbanPop <- hisUrbanPop2 + ggtitle("Murder Rate Histogram")
+ hisUrbanPop <- hisUrbanPop2 + ggtitle("Urban Population Histogram")
  hisUrbanPop
+ 
+ hisRape1 <- ggplot(censusArrestsFinal, aes(x=Murder)) 
+ hisRape2 <- hisRape1 + geom_histogram(binwidth=2,color="black", fill="white")
+ hisRape <- hisRape2 + ggtitle("Rape Rate Histogram")
+ hisRape
