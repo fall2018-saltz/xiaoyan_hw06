a84fe5f4a4716f1d6d61072972b5ec5f748ac1d7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 15:24:58 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -8,5 +8,5 @@ boxplotMurder
 
 #explaining which visualization (boxplot or histogram) you thought was more helpful 
 # I think histogram is more helpful in terms of showing the distribution of one 
-#variable. For comparing the distribution between variables, boxplot might be doing
+#variable. However, histogram might be misleading if the bin width was not appropriately For comparing the distribution between variables, boxplot might be doing
 # a better job.
