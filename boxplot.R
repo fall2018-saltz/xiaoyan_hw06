c419cc8e5ae96d45d959d15661773524fb99b640
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 15:17:45 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -6,7 +6,5 @@ boxplotPop
 boxplotMurder <- ggplot(censusArrestsFinal, aes(x=factor(0),Murder))+geom_boxplot()
 boxplotMurder
 
-head(censusArrestsFinal)
-
 #explaining which visualization (boxplot or histogram) you thought was more helpful 
-# 
+# I think histogram is more helpful in terms of showing the distribution of one 
