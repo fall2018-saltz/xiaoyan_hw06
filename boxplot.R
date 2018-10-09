212845461864980c027e852e198c851af6754a3d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 15:14:11 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -6,5 +6,7 @@ boxplotPop
 boxplotMurder <- ggplot(censusArrestsFinal, aes(x=factor(0),murder))+geom_boxplot()
 boxplotMurder
 
+head(censusArrestsFinal)
+
 #explaining which visualization (boxplot or histogram) you thought was more helpful 
 # 
