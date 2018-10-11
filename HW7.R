2871e9196d312f96c6da375bbe27008c2fed4341
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:07:33 -0400

    Saving HW7.R

--- HW7.R
+++ HW7.R
@@ -12,7 +12,3 @@ censusArrestsHW7
 
 censusArrests <- merge(cleanCensus,arrests,by= "row.names", all= TRUE)
 
-
-2)	Add the area of each state, and the center of each state, 
-to the merged dataframe, 
-using the ‘state.area’, ‘state.center’ and ‘state.name’ vectors
