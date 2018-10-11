9631eabd4d28b78ff134e4063fb50a65310184ed
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:13:57 -0400

    Saving HW7.R

--- HW7.R
+++ HW7.R
@@ -3,7 +3,7 @@ library("ggplot2")
 library("maps")
 library("ggmap")
 
-censusArrestsHW7 <- censusArrestsFinal
+censusArrestsHW7 <-censusArrestsFinal
 colnames(cesnsusArrestsHW7) <-c(censusArrestsHW7,state.area,state.center,state.name) 
 
 2)	Add the area of each state, and the center of each state, 
