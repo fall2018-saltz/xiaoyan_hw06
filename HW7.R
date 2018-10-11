5f668000401bba3b086381e162d53a2b5ec541d4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:04:21 -0400

    Saving HW7.R

--- HW7.R
+++ HW7.R
@@ -4,6 +4,7 @@ library("maps")
 library("ggmap")
 
 censusArrestsHW7 <- censusArrestsFinal
+cesnsusArrestsHw7 <- 
 
 2)	Add the area of each state, and the center of each state, 
 to the merged dataframe, 
