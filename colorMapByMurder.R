3a26da107af80825eac4cba0c5bd121ced41ca42
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:44:17 -0400

    Saving colorMapByMurder.R

--- colorMapByMurder.R
+++ colorMapByMurder.R
@@ -1,5 +1,19 @@
 
+library("ggplot2")
+library("maps")
+library("ggmap")
+
+us <- map_data("state")
+
+# lowercase the state names in order to make the ggplot function works
+DFhw7$stateName <- tolower(DFhw7$stateName)
  
+ # a map named mapColor is created 
+ mapColor <- ggplot(DFhw7, aes(map_id = DFhw7$stateName)) +  
+                geom_map(map = us, fill=DFhw7$state.area) + 
+                expand_limits(x = DFhw7$x, y = DFhw7$y) +
+                coord_map() +  ggtitle("Map By Area in USA")
+mapColor
   
  dfStates <- readCensus()
  # make sure everything is lowercase
