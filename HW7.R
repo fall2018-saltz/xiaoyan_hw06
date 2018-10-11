e6b9032dc6f09eb5e096d91c6e9732e8782be2ee
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:13:36 -0400

    Saving HW7.R

--- HW7.R
+++ HW7.R
@@ -7,8 +7,8 @@ DF <- data.frame(state.area, state.center,state.name)
 
 DF$stateName <- DF$state.name
 head(DF)
-censusArrestsHW7 <-merge(censusArrestsFinal,subDF,by= "stateName", all= TRUE)
-
+DFhw7 <-merge(censusArrestsFinal,DF,by= "stateName", all= TRUE)
+head(DFhw7)
 censusArrestsHW7 
 
 censusArrests <- merge(cleanCensus,arrests,by= "row.names", all= TRUE)
