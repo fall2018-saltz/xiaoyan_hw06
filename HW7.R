42319eff8542ed511cc4c5b4d26b009d773307e8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:12:22 -0400

    Saving HW7.R

--- HW7.R
+++ HW7.R
@@ -5,7 +5,7 @@ library("ggmap")
 
 DF <- data.frame(state.area, state.center,state.name)
 head(DF)
-DF$state.name <- DF$stateName
+DF$stateName <- DF$state.name
 
 censusArrestsHW7 <-merge(censusArrestsFinal,subDF,by= "stateName", all= TRUE)
 
