0caf582d9b91250b0cb30d14bbe2585036321ae7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 22:59:24 -0400

    Saving StepD.R

--- StepD.R
+++ StepD.R
@@ -1,11 +1,8 @@
 
-mapCircleByPopNE <- mapColorByMurder + 
-                geom_point(x = DFhw7$x, y = DFhw7$y, color="white",
-                aes(size=DFhw7$population))
-mapCircleByPopNE
+library("ggmap")
 
-latlon <- geocode("")
+latlon <- geocode("newyork,ny")
 mapCircleByPopNE <- mapColorByMurder + 
-                geom_point(x = latlon$lon, y = DFhw7$y, color="white",
+                geom_point(x = latlon$lon, y = latlon$lat, color="white",
                 aes(size=DFhw7$population))
 mapCircleByPopNE
