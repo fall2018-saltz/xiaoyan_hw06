821aa54048398c0b435b08af16f288310d7fb843
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 22:12:24 -0400

    Saving colorMapByMurder.R

--- colorMapByMurder.R
+++ colorMapByMurder.R
@@ -20,7 +20,8 @@ library("RColorBrewer")
 
  mapCircleByPop <- ggplot(DFhw7, aes(map_id = DFhw7$stateName)) +  
                 geom_map(map = us, fill=DFhw7$Murder) + 
-                geom_point(x = DFhw7$x, y = DFhw7$y, size=DFhw7$population)
+                geom_point(x = DFhw7$x, y = DFhw7$y, 
+                size=DFhw7$population) +
                 expand_limits(x = DFhw7$x, y = DFhw7$y) +
                 coord_map() +  ggtitle("Map By Murder Reate in USA")
 mapCircleByPop
