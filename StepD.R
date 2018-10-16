f51762ca140ca33b20b555ffc52c20d4b8427939
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 22:57:15 -0400

    Saving StepD.R

--- StepD.R
+++ StepD.R
@@ -3,3 +3,8 @@ mapCircleByPopNE <- mapColorByMurder +
                 geom_point(x = DFhw7$x, y = DFhw7$y, color="white",
                 aes(size=DFhw7$population))
 mapCircleByPopNE
+
+mapCircleByPopNE <- mapColorByMurder + 
+                geom_point(x = latlon$lon, y = DFhw7$y, color="white",
+                aes(size=DFhw7$population))
+mapCircleByPopNE
