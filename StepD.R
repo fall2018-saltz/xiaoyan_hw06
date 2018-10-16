503925c68d6718b499aa54e954af6809e2f38082
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 23:01:55 -0400

    Saving StepD.R

--- StepD.R
+++ StepD.R
@@ -2,6 +2,12 @@
 library("ggmap")
 library("gglots")
 
+mapColorByMurder <- ggplot(DFhw7, aes(map_id = DFhw7$stateName)) +  
+                geom_map(map = us, fill=DFhw7$Murder) + 
+                expand_limits(x = DFhw7$x, y = DFhw7$y) +
+                coord_map() +  ggtitle("Map By Murder Rate in USA")
+mapColorByMurder
+
 latlon <- geocode("newyork,ny")
 mapCircleByPopNE <- mapColorByMurder + 
                 geom_point(x = latlon$lon, y = latlon$lat, color="white",
