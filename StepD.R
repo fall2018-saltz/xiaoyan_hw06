4de12fd8fd2590a7ad6578059eb8884cc9a26715
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:40:54 -0400

    Saving StepD.R

--- StepD.R
+++ StepD.R
@@ -18,7 +18,8 @@ mapCircleByPop
 
 latlon <- geocode("newyork,ny")
 mapCircleByPopNE <- ggplot(DFhw7, aes(map_id = DFhw7$stateName)) +  
-                geom_map(map = us, aes(color=DFhw7$Murder,fill=DFhw7$Murder) + 
-                expand_limits(x = DFhw7$x, y = DFhw7$y) +
-                coord_map() +  ggtitle("Map By Murder Rate in USA")
+                geom_map(map = us, aes(color=DFhw7$Murder,
+                fill=DFhw7$Murder) + 
+                expand_limits(x =us$long, y = us$lat) +
+                coord_map(xlim=c(-64,-84),ylim=(30.71,50.71) +  ggtitle("Map By Murder Rate in USA")
 mapCircleByPopNE
