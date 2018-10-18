7ea13f56ae891b09714e3896772ad69f730b61db
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:45:46 -0400

    Saving StepD.R

--- StepD.R
+++ StepD.R
@@ -8,7 +8,7 @@ us <- map_data("state")
 mapCircleByPopNE <- ggplot(DFhw7, aes(map_id = DFhw7$stateName)) +  
                 geom_map(map = us, aes(color=DFhw7$Murder,
                 fill=DFhw7$Murder) + 
-                expand_limits(x =us$long, y = us$lat) +
+                expand_limits(x =us$lon, y = us$lat) +
                 coord_map(xlim=c(-64,-84),ylim=(30.71,50.71) +
                 ggtitle("Map for North East in the USA") +
                 geom_point(x=DFhw7$x,y=DFhw7$y, color="white", 
